c1c9362ca852037b7dcebbd6772e4d17a0821900
mohdi <mohdinzamam12125@gmail.com>
2023-11-24 21:43:05 +0530

    Restart the project

--- app/src/main/java/com/example/newsapi/internetCheck/ResourcesState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newsapi.internetCheck
-
-import com.example.newsapi.data.entity.NewsData
-
-//sealed interface ResourcesState {
-//    object Loading : ResourcesState
-//    data class Success(val data : NewsData): ResourcesState
-//    object Error: ResourcesState
-//}
